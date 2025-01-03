b9461893c596013d01e80bba8d4fc890c7838530
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-01-03 13:08:06 +0530

    develop headers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,25 @@
 import "./App.css";
 
 function App() {
+  const reqBody = {
+    username: "isuranga",
+    password: "asdfasdf",
+  };
+
+  fetch("api/auth/login", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "post",
+    body: JSON.stringify(reqBody),
+  })
+    .then((response) => Promise.all([response.json(), response.headers]))
+    .then(([body, headers]) => {
+      const authValue = headers.get("authorization");
+      console.log(authValue);
+      console.log(body);
+    });
+
   return <div className="App"></div>;
 }
 
