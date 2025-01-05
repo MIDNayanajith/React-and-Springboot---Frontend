b89fd2415e704933017dc15452d3cbe5477010c9
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-01-05 12:52:53 +0530

    do some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
-<<<<<<< Updated upstream
-=======
 import { useLocalState } from "./util/UseLocalStorage";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./Dashboard";
+import Dashboard from "./Dashboard"; // Import your Dashboard component
 import HomePage from "./HomePage";
-import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
->>>>>>> Stashed changes
+import Login from "./Login";
 
 function App() {
-  const reqBody = {
-    username: "isuranga",
-    password: "asdfasdf",
-  };
+  const [jwt, setJwt] = useLocalState("", "jwt");
 
-<<<<<<< Updated upstream
-  fetch("api/auth/login", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "post",
-    body: JSON.stringify(reqBody),
-  })
-    .then((response) => Promise.all([response.json(), response.headers]))
-    .then(([body, headers]) => {
-      const authValue = headers.get("authorization");
-      console.log(authValue);
-      console.log(body);
-    });
-
-  return <div className="App"></div>;
-=======
   // useEffect(() => {
   //   if (!jwt) {
   //     const reqBody = {
@@ -39,7 +17,8 @@ function App() {
   //       password: "asdfasdf",
   //     };
 
-  //     fetch("api/auth/login", {
+  //     fetch("http://localhost:5000/api/auth/login", {
+  //       // Replace with your API's base URL
   //       headers: {
   //         "Content-Type": "application/json",
   //       },
@@ -48,11 +27,15 @@ function App() {
   //     })
   //       .then((response) => Promise.all([response.json(), response.headers]))
   //       .then(([body, headers]) => {
-  //         setJwt(headers.get("authorization"));
-  //         console.log("WE HAVE JWT: ${jwt}");
+  //         const token = headers.get("authorization");
+  //         setJwt(token);
+  //         console.log(`WE HAVE JWT: ${token}`); // Corrected template literal
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error during login:", error);
   //       });
   //   }
-  // }, []);
+  // }, [jwt]);
 
   useEffect(() => {
     console.log(`JWT IS : ${jwt}`);
@@ -73,7 +56,6 @@ function App() {
       <Route path="/login" element={<Login />} />
     </Routes>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
