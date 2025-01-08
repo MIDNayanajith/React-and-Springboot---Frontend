db7b4d2a7c2bbb66f4c0a6ac0130610e702516ee
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-01-08 12:27:59 +0530

    create assignment section

--- frontend/src/Dashboard/index.js
+++ frontend/src/Dashboard/index.js
@@ -4,10 +4,39 @@ import { useLocalState } from "../util/UseLocalStorage";
 const Dashboard = () => {
   const [jwt, setJwt] = useLocalState("", "jwt");
 
+  function createAssignment() {
+    fetch("api/assignments", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        if (response.status === 200) return response.json();
+      })
+      .then((assignment) => {
+        window.location.href = `assignments/${assignment.id}`;
+      });
+  }
   return (
-    <div>
-      <h1>This is dashboard</h1>
-      <div>JWT VALUE IS : {jwt}</div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh", // Full viewport height for centering
+        textAlign: "center", // Center-align text
+      }}
+    >
+      <h1>Welcome To Dashboard</h1>
+      <div style={{ margin: "2em" }}>
+        <button onClick={() => createAssignment()}>
+          {" "}
+          Submit new Assignment{" "}
+        </button>
+      </div>
     </div>
   );
 };
