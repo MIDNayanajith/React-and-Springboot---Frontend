8243e03c741c0e98826b2069950a3de3e62efafd
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-01-11 12:18:52 +0530

    create assignment submission

--- frontend/src/Dashboard/index.js
+++ frontend/src/Dashboard/index.js
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocalState } from "../util/UseLocalStorage";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const [jwt, setJwt] = useLocalState("", "jwt");
+  const [assignments, setAssignments] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/assignments", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      method: "GET",
+    })
+      .then((response) => {
+        if (response.status === 200) return response.json();
+      })
+      .then((assignmentsData) => {
+        setAssignments(assignmentsData);
+      });
+  }, []);
 
   return (
+<<<<<<< Updated upstream
     <div>
       <h1>This is dashboard</h1>
       <div>JWT VALUE IS : {jwt}</div>
+=======
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh", // Full viewport height for centering
+        textAlign: "center", // Center-align text
+      }}
+    >
+      <h1>Welcome To Dashboard</h1>
+      <div style={{ margin: "2em" }}>
+        {assignments ? (
+          assignments.map((assignment) => (
+            <div>
+              <Link to={`/assignments/${assignment.id}`}>
+                Assignment Id : {assignment.id}
+              </Link>
+            </div>
+          ))
+        ) : (
+          <></>
+        )}
+
+        <button onClick={() => createAssignment()}>
+          {" "}
+          Submit new Assignment{" "}
+        </button>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 };
